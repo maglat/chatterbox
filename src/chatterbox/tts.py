f2b91e258c7ef0918425b533c5f0743da803386f
maglat <44977256+maglat@users.noreply.github.com>
2025-06-25 20:41:07 +0200

    Update tts.py
    Use the German fine tuned model
    
    https://huggingface.co/stlohrey/chatterbox_de/tree/main

--- src/chatterbox/tts.py
+++ src/chatterbox/tts.py
@@ -16,7 +16,7 @@ from .models.voice_encoder import VoiceEncoder
 from .models.t3.modules.cond_enc import T3Cond
 
 
-REPO_ID = "ResembleAI/chatterbox"
+REPO_ID = "stlohrey/chatterbox_de"
 
 
 def punc_norm(text: str) -> str:
